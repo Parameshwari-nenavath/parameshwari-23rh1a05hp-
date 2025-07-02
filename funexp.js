e9b4f8677ad87d553b2d9d1573c757693a804b72
Parameshwari-nenavath <parminenavath758@gmail.com>
2025-07-02 18:50:45 +0530

    Add files via upload

---
+++ funexp.js
@@ -0,0 +1,14 @@
+function welcomeMessage(){
+    console.log("Welcome to nenavath parameshwari art");
+}
+function add(a,b){
+    return a+b;
+
+}
+welcomeMessage();
+console.log(add(10,20));
+const mul=(a,b)=>(a*b);
+console.log(mul(4,7));
+
+const greet=()=>("Hello hyderabad");
+console.log(greet())
---
+++ object.js
@@ -0,0 +1,3 @@
+let user={name:"Parameshwari",dept:"cse",age:21};
+console.log(user);
+console.log(user.name);
